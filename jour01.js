ddd0b2f8a9426887f03838c02e8a1dd43ed0f77a
Djibril <djibrilsamassa.pro@gmail.com>
2021-12-13 14:16:04 +0100

    final commit

---
+++ jour01.js
@@ -0,0 +1,38 @@
+// exercice 01
+console.log("Hello World !");
+
+// exercice 02
+var test = "My name is Djibril";
+console.log(test);
+
+//exercice 03
+var name = "Djibril";
+console.log("Nice to meet you " + name)
+
+//exercice 04
+var testlength= "I'm very long !";
+console.log(testlength.length);
+
+//exercice 05
+var food = "croissant is meh";
+console.log(food);
+food = food.replace("meh","so good");
+console.log(food);
+
+//exercice 06
+var basic = "This is so cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic);
+console.log(basicUp);
+console.log(basicDown);
+
+//exercice 07
+var word = "banana";
+var letters = word.split("");
+console.log(letters);
+
+//exercice 08
+var age = "20";
+var template = "I'm " + age + " years old";
+console.log(template);
