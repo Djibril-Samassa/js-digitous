1322b043595b261f449d12045480a190fa7068b2
Djibril <djibrilsamassa.pro@gmail.com>
2021-12-16 15:59:51 +0100

    jour 4 commit

---
+++ jour04/exercice04.js
@@ -0,0 +1,15 @@
+function compare (num1, num2){
+    if(num1 > num2){
+        console.log(num1 + " is bigger");
+    }
+    else if (num2 > num1) {
+        console.log(num2 + " is bigger");
+    }
+    else{
+        console.log("both are the same");
+    }
+}
+
+compare(5,7);
+compare(7,5);
+compare(7,7);
---
+++ jour04/exercice05.js
@@ -0,0 +1,11 @@
+function addUp(num){
+
+    let result = num;
+
+    for( let i = 0; i < num; i++){
+        result = result + i;
+        console.log(result);
+    }
+}
+
+addUp(12)
