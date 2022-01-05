f861b4966eb060530b9ead003a0cca43e8610c1b
Djibril <djibrilsamassa.pro@gmail.com>
2022-01-05 15:20:31 +0100

    node commit

--- jour05/exercice01.js
+++ jour05/exercice01.js
@@ -1,13 +1,27 @@
 
-function calculate(string, nb1, nb2) {
+function calculate(nb1, string, nb2) {
 
 
     nb1 = parseInt(process.argv[2]);
-    nb2 = parseInt(process.argv[3])
+    nb2 = parseInt(process.argv[4])
 
-    if(string = "+"){
+    if(process.argv[3] == "+"){
         console.log(nb1 + nb2);
     }
+
+    else if(process.argv[3] == "-"){
+        console.log(nb1 - nb2);
+    }
+
+    else if( process.argv[3] == "*"){
+        console.log(nb1 * nb2);
+    }
+
+    else if(process.argv[3] == "/"){
+        console.log(nb1 / nb2);
+    }
+
+
 
 }
 calculate();
