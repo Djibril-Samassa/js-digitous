81e86accb983434fb1bcd93a12f3f323447c9073
Djibril <djibrilsamassa.pro@gmail.com>
2022-01-05 15:10:15 +0100

    node commit

--- jour05/exercice01.js
+++ jour05/exercice01.js
@@ -1,9 +1,9 @@
 
-function calculate(nb1, string, nb2) {
+function calculate(string, nb1, nb2) {
 
 
-    nb1 = process.argv[2]
-    nb2 = process.argv[3]
+    nb1 = parseInt(process.argv[2]);
+    nb2 = parseInt(process.argv[3])
 
     if(string = "+"){
         console.log(nb1 + nb2);
