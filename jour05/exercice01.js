94c0e392e2add6757dc23f3c1cab512db5b24066
Djibril <djibrilsamassa.pro@gmail.com>
2022-01-05 15:01:15 +0100

    node commit

---
+++ jour05/exercice01.js
@@ -0,0 +1,13 @@
+
+function calculate(nb1, string, nb2) {
+
+
+    nb1 = process.argv[2]
+    nb2 = process.argv[3]
+
+    if(string = "+"){
+        console.log(nb1 + nb2);
+    }
+
+}
+calculate();
