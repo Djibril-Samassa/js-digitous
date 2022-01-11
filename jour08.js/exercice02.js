ff49ef2c904e5d33916d9e9e607c5c75f679acb4
Djibril <djibrilsamassa.pro@gmail.com>
2022-01-11 16:14:10 +0100

    Chuck Norris Commit

---
+++ jour08.js/exercice02.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+
+function getFact(){
+    axios
+.get("https://api.chucknorris.io/jokes/random")
+.then(function(res){
+    console.log(res.data.value);
+})
+}
+
+getFact();
